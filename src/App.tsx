54a2f880f2dc09709d8b3658445ef8d7499bcf34
Jordi Mestres Lopez <jordi.mestres.l@gmail.com>
2023-01-25 13:52:06 +0100

    Fetch

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Routes, Route } from 'solid-app-router';
+import { Component, createEffect, createSignal } from 'solid-js';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import SavedRepos from './pages/SavedRepos';
+
+const [username, setUsername] = createSignal('ebenezerdon');
+const [repos, setRepos] = createSignal([]);
+
+const App: Component = () => {
+  createEffect(async () => {
+    const res = await fetch(
+      `https://api.github.com/users/${username()}/repos?sort=created`
+    );
+    setRepos(await res.json());
+  });
+
+  return (
+    <div class="container">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/savedrepos" element={<SavedRepos />} />
+      </Routes>
+    </div>
+  );
+};
+
+export { username, setUsername, repos };
+export default App;
